995b5fcc0ca3075f7f2e0ccc9a8120625edb0a56
Bhareth Narayann Prasad <bhareth@bluejeans.com>
2020-05-28 13:00:15 +0530

    formatted doc

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomImages1 = "dice" + randomNumber1 + ".png";
 
-
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src" , randomImages1);
+image1.setAttribute("src", randomImages1);
 
-var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImages2 = "dice" + randomNumber2 + ".png";
 
-
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src" , randomImages2);
+image2.setAttribute("src", randomImages2);
 
-if(randomImages1 > randomImages2){
-
-    document.querySelector("h1").innerHTML = "Player 1 Wins! 🤩";
-} else if(randomImages2 > randomImages1){
-
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🤩";
+if (randomImages1 > randomImages2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins! 🤩";
+} else if (randomImages2 > randomImages1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🤩";
 } else {
-    document.querySelector("h1").innerHTML = "It's a draw! 🍻";
+  document.querySelector("h1").innerHTML = "It's a draw! 🍻";
 }
-
-
-
